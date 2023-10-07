f3671352e07318b1dc2dac5ce58d60db8b689030
Pedro <pedrojuniorifpb@gmail.com>
2023-10-07 04:27:51 -0300

    Estrutura inicial da pagina de demandas

---
+++ src/components/CardDemand.jsx
@@ -0,0 +1,14 @@
+export const CardDemand = () => {
+  return (
+    <div className="">
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo veritatis
+        dignissimos dolorum eligendi voluptas odio eveniet incidunt dolores quia
+        temporibus, iste voluptatum, ratione, corrupti voluptates quisquam
+        voluptatibus iusto eum sed?
+      </p>
+      <img src="" alt="Joinha" />
+      <p className=""></p>
+    </div>
+  );
+};
--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Main } from "./pages/Main";
+
+import { Services } from "./pages/Services.jsx";
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: Main,
+      element: <Services />,
     },
 ]);
 
