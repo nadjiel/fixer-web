90e21d6a135c0bea0ebd4a72a7867d8107182ab9
yolisses <iamurise@gmail.com>
2023-10-07 05:31:23 -0300

    move navbar to bottom

--- src/nav/NavBar.jsx
+++ src/nav/NavBar.jsx
@@ -2,16 +2,19 @@ import { Link } from "react-router-dom";
 
 export function NavBar() {
   return (
-    <div className="flex-row mt-auto text-sm justify-between">
-      <Link className="flex-1 text-center" to={"/demands"}>
-        Demandas
-      </Link>
-      <Link className="flex-1 text-center" to={"/services"}>
-        Serviços
-      </Link>
-      <Link className="flex-1 text-center" to={"/account"}>
-        Conta
-      </Link>
-    </div>
+    <>
+      <div className="h-10"></div>
+      <div className="h-10 flex-row mt-auto text-sm justify-between fixed bottom-0 w-full">
+        <Link className="flex-1 text-center p-2" to={"/demands"}>
+          Demandas
+        </Link>
+        <Link className="flex-1 text-center p-2" to={"/services"}>
+          Serviços
+        </Link>
+        <Link className="flex-1 text-center p-2" to={"/account"}>
+          Conta
+        </Link>
+      </div>
+    </>
   );
 }
