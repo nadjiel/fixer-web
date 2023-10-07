6d9577c77f3fe7b5d72e41984c12180adb6b6a23
Pedro <pedrojuniorifpb@gmail.com>
2023-10-07 17:34:32 -0300

    Update Files

--- src/router.jsx
+++ src/router.jsx
@@ -10,6 +10,7 @@ import { ServicesAllPage } from "./service/listServices/ServicesAllPage";
 import { ServicePage } from "./service/ServicePage";
 import { AccountDemandsPage } from "./account/AccountDemandsPage";
 import { AccountLikesPage } from "./account/AccountLikesPage";
+import { EditAccountPage } from "./account/EditAccountPage";
 
 const router = createBrowserRouter([
   {
@@ -43,6 +44,14 @@ const router = createBrowserRouter([
   {
     path: "/services/:service/create",
     element: <ServiceCreatePage />,
+  },
+  {
+    path: "/account",
+    element: <AccountPage />,
+  },
+  {
+    path: "/account/edit",
+    element: <EditAccountPage />,
   },
   {
     path: "/signIn",
