64bef6a6a420501ec9eb8761fdee6103ea01ec25
yolisses <iamurise@gmail.com>
2023-10-07 12:49:58 -0300

    add create demand button

--- src/demand/DemandsPage.jsx
+++ src/demand/DemandsPage.jsx
@@ -3,6 +3,7 @@ import { api } from "../api";
 import { NavBar } from "../navBar/NavBar";
 import { DemandInput } from "./DemandInput";
 import { DemandList } from "./DemandList";
+import { CreateDemandButton } from "./CreateDemandButton";
 
 export function DemandsPage() {
   const [demands, setDemands] = useState();
@@ -19,8 +20,10 @@ export function DemandsPage() {
 
   return (
     <div>
-      <DemandInput reload={getDemands} />
       {demands ? <DemandList demands={demands} /> : <div>carregando</div>}
+      <div className="fixed bottom-20 z-10 right-10">
+        <CreateDemandButton />
+      </div>
       <NavBar active={"Demandas"} />
     </div>
   );
