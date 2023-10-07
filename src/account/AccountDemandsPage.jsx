465811e901b2f8c3e624de4439f9ac53817a1e16
yolisses <iamurise@gmail.com>
2023-10-07 19:22:41 -0300

    make loading more nested on accountDemandsPage

--- src/account/AccountDemandsPage.jsx
+++ src/account/AccountDemandsPage.jsx
@@ -19,16 +19,16 @@ export function AccountDemandsPage() {
     getDemands();
   }, []);
 
-  if (!demands) {
-    return <div>carregando...</div>;
-  }
-
   return (
     <div className="items-center">
       <div className="max-w-xl w-full">
         <div className="gap-4">
           <div className="text-primary text-lg">Demandas feitas</div>
-          <DemandList demands={demands}></DemandList>
+          {demands ? (
+            <DemandList demands={demands}></DemandList>
+          ) : (
+            <div>carregando...</div>
+          )}
         </div>
       </div>
       <NavBar active={"Conta"} />
