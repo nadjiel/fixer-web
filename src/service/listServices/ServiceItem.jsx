6d3370ee51d1d087f6c02698a158acf987c7315e
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2023-10-07 13:56:56 -0300

    making the service item link go to the services page instead of the service creationg

--- src/service/listServices/ServiceItem.jsx
+++ src/service/listServices/ServiceItem.jsx
@@ -2,7 +2,7 @@ import { IoHeartCircle } from "react-icons/io5";
 
 export function ServiceItem({ data }) {
   return (
-    <a href={`services/${data}/create`} className="bg-[#EAF1F9] rounded-xl py-8 px-6 w-full flex-row align-center gap-3 cursor-pointer">
+    <a href={`services/${data}`} className="bg-[#EAF1F9] rounded-xl py-8 px-6 w-full flex-row align-center gap-3 cursor-pointer">
       <IoHeartCircle color="#B3261E" size={32} />
       <h1 className="text-xl text-[#B3261E]">{data}</h1>
     </a>
