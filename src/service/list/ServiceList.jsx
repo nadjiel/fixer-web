85e16a12946c52a84e68d5ec1b1b96f63b3df14e
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2023-10-07 13:43:28 -0300

    passing service id from the service list to the service page

--- src/service/list/ServiceList.jsx
+++ src/service/list/ServiceList.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import { ServiceItem } from "./ServiceItem";
 
 export function ServiceList({ services }) {
   return (
     <div className="gap-2">
       {services.map((service) => (
-        <ServiceItem service={service} />
+        <Link to={`/services/service/${service.id}`}> {/* TODO specify service */}
+          <ServiceItem service={service} />
+        </Link>
       ))}
     </div>
   );
