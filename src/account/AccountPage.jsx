5436078e805e5607aea31c34f9bed340171498af
yolisses <iamurise@gmail.com>
2023-10-07 07:56:49 -0300

    remove fontawessome specific package

--- src/account/AccountPage.jsx
+++ src/account/AccountPage.jsx
@@ -1,11 +1,11 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FaUser } from "react-icons/fa";
 import { NavBarComponent } from "../navBar/NavBarComponent.jsx";
 
 export const AccountPage = () => {
   return (
     <div className="h-full">
       <div>
-        <FontAwesomeIcon icon="fa-solid fa-user" />
+        <FaUser />
         <h1>Home</h1>
       </div>
       <button className="bg-blue-400 px-4 py-2">Likes</button>
