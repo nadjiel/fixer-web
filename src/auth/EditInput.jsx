2f703f2a975a5085fad82d53f912f29c49e04235
Pedro <pedrojuniorifpb@gmail.com>
2023-10-08 01:59:45 -0300

    Adicionando pequenas modificações no css da pagina

--- src/auth/EditInput.jsx
+++ src/auth/EditInput.jsx
@@ -1,15 +1,24 @@
-export function EditInput({ inputName, typeInput, idInput, placeholder, Icon  }) {
-    return (
-      <div className="gap-2">
-        <label htmlFor={idInput} className="font-normal text-base">{inputName}</label>
-        <Icon size={24} className={"relative -bottom-10 left-3 h-6 text-secondary-400"}/>
+export function EditInput({
+  inputName,
+  typeInput,
+  idInput,
+  placeholder,
+  Icon,
+}) {
+  return (
+    <div className="gap-2">
+      <label htmlFor={idInput} className="font-normal text-base">
+        {inputName}
+      </label>
+      <div className="flex-row w-full">
+        <Icon size={24} className={"-mr-9 self-center h-6 z-10 text-secondary-400"} />
         <input
-          className="bg-secondary-200 py-2 px-11 rounded-3xl"
+          className="bg-secondary-200 w-full z-0 py-2 px-11 rounded-3xl"
           placeholder={placeholder}
           type={typeInput}
           id={idInput}
         />
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
