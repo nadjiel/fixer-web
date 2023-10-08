8e787b897fe772f1de7daeab1ececcaef3f2104e
yolisses <iamurise@gmail.com>
2023-10-08 11:59:35 -0300

    add gap

--- src/demand/DemandList.jsx
+++ src/demand/DemandList.jsx
@@ -2,7 +2,7 @@ import { DemandItem } from "./DemandItem";
 
 export function DemandList({ demands }) {
   return (
-    <div className="p-2">
+    <div className="p-2 gap-2">
       {demands.map((demand) => {
         return <DemandItem demand={demand} key={demand.id} />;
       })}
