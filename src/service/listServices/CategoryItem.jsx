0a95221d23291f7fca01a68a3231a92be4ce8120
yolisses <iamurise@gmail.com>
2023-10-07 23:28:12 -0300

    style CategoryItem

--- src/service/listServices/CategoryItem.jsx
+++ src/service/listServices/CategoryItem.jsx
@@ -6,9 +6,9 @@ export function CategoryItem({ category }) {
   return (
     <Link
       to={`/services/category/${category}`}
-      className="bg-[#EAF1F9] font-medium rounded-xl py-8 px-6 w-full flex-row align-center gap-3 cursor-pointer"
+      className="bg-[#EAF1F9] font-medium rounded-xl p-4 w-full flex-row align-center gap-3 cursor-pointer"
     >
-      <IoHeartCircle color={primaryColor} size={32} />
+      <IoHeartCircle color={primaryColor} size={32} className="shrink-0" />
       <h1 className="text-xl text-primary">{category}</h1>
     </Link>
   );
