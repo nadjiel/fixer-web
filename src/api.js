71e3f5685759f5835abf09df84cc3c40681738bb
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2025-11-03 22:07:40 -0300

    moved hardcoded api url to env file

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ import Cookies from "js-cookie";
 const access = Cookies.get("access");
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: {
     Authorization: `Bearer ${access}`,
   },
