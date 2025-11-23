e170c7ce0ece5ce9b44335fcc6aefeaeafbf5b32
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2025-11-23 14:25:31 -0300

    :books: docs: described Loader page

--- src/common/pages/loader.jsx
+++ src/common/pages/loader.jsx
@@ -1,5 +1,8 @@
 import { AiOutlineLoading3Quarters as LoadingIcon } from "react-icons/ai";
 
+/**
+ * A page that displays a loading icon in the center.
+ */
 export default function Loader() {
   return (
     <div className="flex-1 justify-center items-center">
