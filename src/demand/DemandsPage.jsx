94affccc18f9ef8ec8b13b33a5f1cc0a744bd747
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2025-11-23 15:00:54 -0300

    :sparkles: feat: improved loading ux in DemandsPage

--- src/demand/DemandsPage.jsx
+++ src/demand/DemandsPage.jsx
@@ -3,9 +3,11 @@ import { api } from "../api";
 import { NavBar } from "../navBar/NavBar";
 import { DemandList } from "./DemandList";
 import { AddDemandButton } from "./AddDemandButton";
+import { Loader } from "../common/pages";
+import illustration from "../assets/no-demands.svg"
 
 export function DemandsPage() {
-  const [demands, setDemands] = useState();
+  const [demands, setDemands] = useState(null);
 
   async function getDemands() {
     const res = await api.get("/demands/");
@@ -19,9 +21,21 @@ export function DemandsPage() {
   }, []);
 
   return (
-    <div className="items-center">
-      <div className="max-w-xl w-full">
-        {demands ? <DemandList demands={demands} /> : <div>carregando</div>}
+    <div className="flex-1 items-center">
+      <div className="flex-1 max-w-xl w-full">
+        {
+          !demands
+            ? <Loader />
+            : demands.length === 0
+            ? (
+              <div className="text-center items-center">
+                <img src={illustration} className="max-w-md w-full" />
+                <h2 className="text-xl font-bold">Tudo OK por aqui!</h2>
+                <p>Não há demandas cadastradas.</p>
+              </div>
+            )
+            : <DemandList demands={demands} />
+        }
         <div className="fixed bottom-20 z-10 right-0 p-5">
           <AddDemandButton />
         </div>
