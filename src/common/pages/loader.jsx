9c007b47708b76b1681a361e15a5f2a40cca969d
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2025-11-23 14:23:11 -0300

    :sparkles: feat: added loading page

---
+++ src/common/pages/loader.jsx
@@ -0,0 +1,9 @@
+import { AiOutlineLoading3Quarters as LoadingIcon } from "react-icons/ai";
+
+export default function Loader() {
+  return (
+    <div className="flex-1 justify-center items-center">
+      <LoadingIcon className="w-16 h-16 text-primary animate-spin" />
+    </div>
+  )
+}
