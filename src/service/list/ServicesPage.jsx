8f1750a2f48a6b996f0e97f773c56d17721e35c9
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2025-11-23 19:47:20 -0300

    :sparkles: feat: improved services page ux

--- src/service/list/ServicesPage.jsx
+++ src/service/list/ServicesPage.jsx
@@ -3,14 +3,17 @@ import { useParams } from "react-router-dom";
 import { api } from "../../api";
 import { NavBar } from "../../navBar/NavBar";
 import { ServiceList } from "./ServiceList";
-import { AddServiceButton } from "./AddServiceButton";
+// import { AddServiceButton } from "./AddServiceButton";
+import { Loader } from "../../common/pages";
+import illustration from "../../assets/no-demands.svg"
 
 export function ServicesPage() {
   const { category } = useParams();
-  const [services, setServices] = useState();
+  const [services, setServices] = useState(null);
 
   async function getServices() {
-    const res = await api.get(`/services?category=${category}`);
+    const res = await api.get(`services?category=${category}`);
+
     setServices(res.data);
   }
 
@@ -19,20 +22,28 @@ export function ServicesPage() {
   }, []);
 
   return (
-    <div>
+    <div className="flex-1">
       <hr />
-      <section className="p-2">
+      <section className="p-2 flex flex-1 flex-col">
         <h1 className="text-xl text-primary">{category}</h1>
-        {services ? (
-          <ServiceList services={services}></ServiceList>
-        ) : (
-          <div>carregando...</div>
-        )}
+        {
+          !services
+            ? <Loader />
+            : services.length === 0
+            ? (
+              <div className="text-center items-center">
+                <img src={illustration} className="max-w-md w-full" />
+                <h2 className="text-xl font-bold">Sem serviços</h2>
+                <p>Não temos serviços dessa categoria ainda.</p>
+              </div>
+            )
+            : <ServiceList services={services} />
+        }
       </section>
-      {/* TODO admin only */}
-      <div className="fixed bottom-20 z-10 right-0 p-5">
+      {/* TODO: admin only */}
+      {/* <div className="fixed bottom-20 z-10 right-0 p-5">
         <AddServiceButton category={ category } />
-      </div>
+      </div> */}
       <NavBar active={"Serviços"} />
     </div>
   );
